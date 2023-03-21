567073e01d0dcab29c2c5c8e62d1cbf5a69cdc92
sayross <saadouchnid@gmail.com>
2023-03-21 12:23:24 -0700

    task 6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_last_digit - prints the last digit
+  *
+  * @n: the number to be checked
+  *
+  * Return: the value of last digit
+  */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
