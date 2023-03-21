6db03b8fcc497635f98ea8022c0277f3c68f12dd
sayross <saadouchnid@gmail.com>
2023-03-21 11:28:23 -0700

    task 5 3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,18 +8,18 @@
 int print_sign(int n)
 {
 	if (n < 0)
-	{;
-		_putchar('-');
+	{
 		return (-1);
+		_putchar('-');
 	}
 	else if (n > 0)
 	{
-		_putchar('+');
 		return (1);
+		-putchar('+');
 	}
 	else
 	{
-		-putchar('0');
 		return (0);
+		_putchar('0');
 	}
 }
