33b3671fb23ecf278f8ba77825f24823adbb7a7c
sayross <saadouchnid@gmail.com>
2023-03-21 11:52:01 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * computes the absolute value of an integer
+ * @n : number
+ * Return: n if + or 0, -n if -
+ */
+int _abs(int n);
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+	_putchar('\n');
+}
