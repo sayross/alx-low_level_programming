2ce227f67aa05c9cdf77a0db07788ab4bff0e055
sayross <saadouchnid@gmail.com>
2023-03-21 16:00:54 -0700

    task 11 3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 			printf("%d, ", i);
 			i++;
 		}
-		printf("%d", 98);
+		printf("%d\n", 98);
 	}
 	else if (n > 98)
 	{
@@ -28,10 +28,10 @@ void print_to_98(int n)
 			printf("%d, ", i);
 			i--;
 		}
-		printf("%d", 98);
+		printf("%d\n", 98);
 	}
 	else
 	{
-		printf("%d", 98);
+		printf("%d\n", 98);
 	}
 }
