5fab7b7ae8bed3f757a1548dec71f27672c1ed4f
sayross <saadouchnid@gmail.com>
2023-03-21 12:56:51 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+  * jack_bauer - prints every minute of the day
+  *
+  * Return: always 0 (Success)
+  */
+
+void jack_bauer(void)
+{
+	int i = 0;
+	int j;
+	int k;
+	int l;
+
+	while (i <= 2)
+	{
+		j = 0;
+		while (j <= 3)
+		{
+			k = 0;
+			while (k <= 5)
+			{
+				l = 0;
+				while (l <= 9)
+				{
+					_putchar(i + 48);
+					_putchar(j + 48);
+					_putchar(':');
+					_putchar(k + 48);
+					_putchar(l + 48);
+					-putchar('\n');
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+
