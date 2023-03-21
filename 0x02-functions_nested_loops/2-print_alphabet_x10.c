260cf55f99a9e94b9295fd07bce13f37da876e85
sayross <saadouchnid@gmail.com>
2023-03-21 10:32:05 -0700

    correct task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 97; j <= 122; j++)
 		{
-			for (j = 97; j <= 122; j++)
-			{
-				_putchar(j);
-			}
-			_putchar('\n');
+			_putchar(j);
 		}
+		_putchar('\n');
+	}
 }
