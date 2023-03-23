86fc1c650b8fa94d54dff2317a71de08065e9554
sayross <saadouchnid@gmail.com>
2023-03-23 09:17:25 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_numbers - prints the numbers from 0 to 9
+  *
+  * Return: void
+  */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
