95a0d87cd8707f021297aafeeb1dac73ff7856d8
sayross <saadouchnid@gmail.com>
2023-03-23 10:04:16 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,16 @@
 void more_numbers(void)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	while (i <= 9)
 	{
+		j = 0;
 		while (j <= 14)
 		{
 			if (j >= 10)
 			{
-				_putchar('1');
+				_putchar((j / 10) + 48);
 			}
 			_putchar((j % 10) + 48);
 			j++;
