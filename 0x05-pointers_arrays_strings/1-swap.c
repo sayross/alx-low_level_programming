976cfe1b328434bc22349a12024fe86d03bbd7a2
sayross <saadouchnid@gmail.com>
2023-03-28 10:58:45 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ *@a: first integer
+ *@b: second integer
+ * Return: void
+ */
+
+swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c = *a;
+	*a = *b;
+	*b = *c;
+}
