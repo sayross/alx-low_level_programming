a7b451475913cf0d9d69ff3e8e2882f4cb6d3236
sayross <saadouchnid@gmail.com>
2023-03-28 20:10:58 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,14 +19,14 @@ void puts_half(char *str)
 	}
 	if (l % 2 == 0)
 	{
-		for (i = l / 2; i <= l; i++)
+		for (i = l / 2; i < l; i++)
 		{
 			_putchar(*(str + i));
 		}
 	}
 	else
 	{
-		for (i = (l - 1) / 2; i <= l; i++)
+		for (i = (l - 1) / 2; i < l; i++)
 		{
 			_putchar(*(str + i));
 		}
