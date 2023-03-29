cf90a4a65e044cc57e5a82e691ba0d2001d2d811
sayross <saadouchnid@gmail.com>
2023-03-28 19:44:56 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	while (*(str + i) != '\0')
+	{
+		if (count % 2 == 0)
+		{
+			_putchar(*(s + i));
+		}
+		i++;
+	}
+}
+
