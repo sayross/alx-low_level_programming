91703c9a16372aa1129cc7bb9a96de0357583a2c
sayross <saadouchnid@gmail.com>
2023-03-30 13:53:44 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,14 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; *(s1 + i) != '\0' || *(s2 + i) != '\0'; i++)
 	{
-		if (*(s1 + i) != *(s2 + i))
+		if (*(s1 + i ) == *(s2 + i))
 		{
-			c = *(s1 + i) - *(s2 + i);
-			return (c);
+			continue;
 		}
-		else
+		else 
 		{
-			return (0);
+			break;
 		}
 	}
+	return (*(s1 + i) - *(s2 + i));
 }
