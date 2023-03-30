07a6441c89316e90f7fbea16126ed82c4e010079
sayross <saadouchnid@gmail.com>
2023-03-30 16:38:25 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string into 1337
+ * @s: string
+ * Return: 1337
+ */
+
+char *leet(char *s)
+{
+
+	int i;
+	int j;
+	char c[] = {a, A, e, E, o, O, t, T, l, L};
+	char n[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (*(s + i) == c[j])
+			{
+				*(s + i) = n[j];
+			}
+		}
+	}
+	return (s);
+}
