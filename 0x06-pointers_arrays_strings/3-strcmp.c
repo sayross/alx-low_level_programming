e72d3e1c3438308406bf5657ebe9790e1fdfb32a
sayross <saadouchnid@gmail.com>
2023-03-30 13:06:10 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - check the code
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 if equal, negative or postive value.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int c;
+
+	while (*(s1 + i) = *(s2 + i))
+	{
+		i++;
+	}
+	c = *(s1 + i) - *(s2 + i);
+	return (c);
+}
