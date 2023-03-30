d73c0887ed573350403671cb2b871a6d0f2396b4
sayross <saadouchnid@gmail.com>
2023-03-29 20:36:46 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,6 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int l = 0;
 	int i;
 
 	for (i = 0; i < n && *(src + i) != '\0'; i++)
@@ -22,4 +21,5 @@ char *_strncpy(char *dest, char *src, int n)
 		*(dest + i) = '\0';
 		i++;
 	}
+	return (dest);
 }
