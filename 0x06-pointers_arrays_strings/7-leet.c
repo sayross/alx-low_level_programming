d25bd0f4951ae1d4252f39a7bbbabd31a2e74ef2
sayross <saadouchnid@gmail.com>
2023-03-30 16:41:09 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,7 +14,7 @@ char *leet(char *s)
 	char c[] = {a, A, e, E, o, O, t, T, l, L};
 	char n[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
 
-	for (i = 0; *(s + i) != 0; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
