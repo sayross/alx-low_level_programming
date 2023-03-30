85fde343cb4826fd79d481678d41d71ca0d86e9f
sayross <saadouchnid@gmail.com>
2023-03-29 18:43:57 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @src: first string
+ * @dest: second string
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int l1 = 0;
+	int l2 = 0;
+	int i;
+	int j;
+
+	while (*(dest + l1) != '\0')
+	{
+		l1++;
+	}
+	while (*(src + l2) != '\0')
+	{
+		l2++;
+	}
+	for (i = 0; i < l1; i++)
+	{
+		_putchar(*(dest + i));
+	}
+	for (j = 0; j <= l2; j++)
+	{
+		_putchar(*(src + j));
+	}
+	_putchar('\n');
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+README File
+C - More pointers, arrays and strings
