ea8e63ea14ee95fbac5dd3f138aa6fde8b250c22
sayross <saadouchnid@gmail.com>
2023-03-30 19:05:45 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ * @n: integer
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i = 1000000000;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * (-1);
+	}
+	if (n < 10)
+	{
+		_putchar(n + 48)
+	}
+	while (i <= 10)
+	{
+		if (i <= n)
+		{
+			_putchar(((n / i) % 10) + 48);
+		}
+		i = i / 10;
+	}
+	_putchar ((n % 10) + 48);
+}
