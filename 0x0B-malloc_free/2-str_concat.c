e5c8168a24d44bcfaadde43dd0af68f6d04bee29
sayross <saadouchnid@gmail.com>
2023-04-11 16:59:34 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,18 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * *str_concat - check the code for ALX School students.
  * @s1: string 1
  * @s2: string 2
  * Return: pointer to the new allocated space of s1 and s2
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int i;
-	int size1 = 0;
-	int size2 = 0;
+	int i, size1 = 0, size2 = 0;
 	char *t;
 
 	while (s1[size1] != '\0')
@@ -25,16 +21,20 @@ char *str_concat(char *s1, char *s2)
 	}
 	t = malloc((size1 + size2 + 1) * sizeof(char));
 	if (t == 0)
-	{
 		return (0);
-	}
-	for (i = 0; i < size1; i++)
-	{
-		t[i] = s1[i];
-	}
-	for (i = 0; i < size2; i++)
-	{
-		t[i + size1] = s2[i];
-	}
+	if (s1 == 0)
+		t = s2;
+	else
+		for (i = 0; i < size1; i++)
+		{
+			t[i] = s1[i];
+		}
+	if (s2 == 0)
+		t = s1;
+	else
+		for (i = 0; i < size2; i++)
+		{
+			t[i + size1] = s2[i];
+		}
 	return (t);
 }
