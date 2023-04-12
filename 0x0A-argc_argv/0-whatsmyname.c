7743653f5f6e0578fb46e5c9db0d477f34636a07
sayross <saadouchnid@gmail.com>
2023-04-11 23:45:19 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,12 @@
 /**
   * main - prints its name, followed by a new line
   * @argv: arguments strings
+  * @argc: number of arguments
   * Return: always 0 (success)
   */
 
 int main(int argc, char **argv)
 {
-	printf("%s\n", argv[argc -1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
