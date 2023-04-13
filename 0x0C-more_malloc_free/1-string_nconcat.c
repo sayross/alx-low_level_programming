c30d7d5820b8ddf9b5e76371100ab3a7061bfb9e
sayross <saadouchnid@gmail.com>
2023-04-13 16:24:04 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,12 +14,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int l1 = 0, l2 = 0, i;
 	char *s;
 
-	if (s1 != NULL)
-		while (s1[l1] != '\0')
-			l1++;
-	if (s2 != NULL)
-		while (s2[l2] != '\0')
-			l2++;
+	if (s1 == NULL)
+		s1 = "";
+	while (s1[l1] != '\0')
+		l1++;
+	if (s2 == NULL)
+		s2 = "";
+	while (s2[l2] != '\0')
+		l2++;
 	if (n < l2)
 	{
 		l2 = n;
