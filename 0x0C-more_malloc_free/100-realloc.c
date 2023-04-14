2dff4163ea60feca18766e880736dd1ca4585e86
sayross <saadouchnid@gmail.com>
2023-04-13 19:41:38 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,12 +32,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	p = malloc(new_size);
-	if (p == NULL)
+	if (new_size > old_size)
 	{
-		return (NULL);
+		p = malloc(new_size);
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		memcpy(p, ptr, new_size);
+		free(ptr);
+		return (p);
 	}
-	memcpy(p, ptr, new_size);
-	free(ptr);
-	return (p);
 }
