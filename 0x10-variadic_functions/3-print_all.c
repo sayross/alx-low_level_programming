f939eaee78fc34e1a0f2fca9ab1e2087b262de4f
sayross <saadouchnid@gmail.com>
2023-04-20 19:57:01 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,7 +30,7 @@ void print_all(const char * const format, ...)
 					printf("%s%d", sep, va_arg(list, int));
 					break;
 				case 'f':
-					printf("%s%f", sep, va_arg(list, double));
+					printf("%s%f", sep, va_arg(list, float));
 					break;
 				case 's':
 					str = va_arg(list, char *);
